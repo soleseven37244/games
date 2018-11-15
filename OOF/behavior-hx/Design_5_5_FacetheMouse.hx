506c0b095e9708ef08f423355612744534d48d43
soleseven37244 <44273234+soleseven37244@users.noreply.github.com>
2018-11-15 13:37:27 -0800

    11/15

---
+++ OOF/behavior-hx/Design_5_5_FacetheMouse.hx
@@ -0,0 +1,120 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.Script.*;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class Design_5_5_FacetheMouse extends ActorScript
+{
+	public var _DistanceX:Float;
+	public var _DistanceY:Float;
+	public var _Facing:Float;
+	public var _OnlyWhenPressed:Bool;
+	
+	
+	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	{
+		super(actor);
+		nameMap.set("Actor", "actor");
+		nameMap.set("Distance X", "_DistanceX");
+		_DistanceX = 0.0;
+		nameMap.set("Distance Y", "_DistanceY");
+		_DistanceY = 0.0;
+		nameMap.set("Facing", "_Facing");
+		_Facing = -90.0;
+		nameMap.set("Only When Pressed", "_OnlyWhenPressed");
+		_OnlyWhenPressed = false;
+		
+	}
+	
+	override public function init()
+	{
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				if((!(_OnlyWhenPressed) || isMouseDown()))
+				{
+					_DistanceX = asNumber(((getScreenX() + getMouseX()) - actor.getXCenter()));
+					propertyChanged("_DistanceX", _DistanceX);
+					_DistanceY = asNumber(((getScreenY() + getMouseY()) - actor.getYCenter()));
+					propertyChanged("_DistanceY", _DistanceY);
+					actor.setAngle(Utils.RAD * ((Utils.DEG * (Math.atan2(_DistanceY, _DistanceX)) - _Facing)));
+				}
+			}
+		});
+		
+	}
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
